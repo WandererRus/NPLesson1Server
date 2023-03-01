639715215c2f47d8c25c893bdca9b353ab9bc291
Fastov Ilya Olegovich <iluxailuxa@gmail.com>
2023-03-01 21:22:22 +0300

    123

--- NPLesson1Client/Program.cs
+++ NPLesson1Client/Program.cs
@@ -1,16 +1,31 @@
 ﻿using System.Net.Sockets;
 using System.Net;
 using System.Text;
+using System.Drawing;
+using System.Diagnostics.Metrics;
+using System.Reflection.Metadata.Ecma335;
+using System.Reflection;
+
 namespace NPLesson1Client
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            System.Timers.Timer timer = new System.Timers.Timer(1000);
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+            while (timer.Enabled) 
+            {
+
+            }
+        }
+        private static void Timer_Elapsed(object? sender, System.Timers.ElapsedEventArgs e)
+        {
             Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
             IPEndPoint point = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 80);
-
-            try { 
+            try
+            {
                 socket.Connect(point);
                 byte[] buffer = new byte[1024];
                 int c;
@@ -20,16 +35,16 @@ namespace NPLesson1Client
                     Console.WriteLine(Encoding.UTF8.GetString(buffer));
                 }
                 while (c > 0);
-                
             }
-            catch(Exception ex) 
+            catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
             }
-            finally { 
+            finally
+            {
                 socket.Shutdown(SocketShutdown.Both);
-                socket.Close(); 
+                socket.Close();
             }
         }
-    }
+    }       
 }
